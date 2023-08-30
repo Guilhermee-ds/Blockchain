483d365e4afc4a2302a3a727b6824252453d5436
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-08-29 21:30:21 -0300

    update and creat modules and edit files index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { BlockChain } from './blockchain'
+import { Console } from "console"
 
-const blockchain = new BlockChain(Number(process.argv[2] || 4))
-const blockNumber = +process.argv[3] || 10
+const dificulty = Number(process.argv[2]) || 4
+const blockchain = new Blockchain(dificulty)
+
+const numBlocks = Number(process.argv[3]) || 10
 let chain = blockchain.chain
 
-for (let i = 1; i <= blockNumber; i++) {
-  const block = blockchain.createBlock(`Block ${i}`)
-  const mineInfo = blockchain.mineBlock(block)
-  chain = blockchain.pushBlock(mineInfo.minedBlock)
+for (let i =1; i < numBlocks; i++) {
+  const block = blockchain.createBlock( `block ${i}` )
+  const mineInfo = blockchain.mineBlocks(block)
+  chain = blockchain.sendBlock(mineInfo.blockMiner)
 }
 
-console.log('--- GENERATED CHAIN ---\n')
+console.log('-- BLOCKCHAIN --')
+
 console.log(chain)
