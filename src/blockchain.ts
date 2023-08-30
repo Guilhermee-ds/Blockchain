2cc9e0b336c1445bb1af916af1bf752e63158ee0
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-08-29 21:36:36 -0300

    update: blockchain.ts and add information fix: name of hashBlock

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,10 +1,17 @@
 export interface Block {
+
+  // pt-br = informação meta dado do bloco.
+  //en = block meta data information.
   header: {
     nonce: number;
-    hasblock: string;
+    hashblock: string;
   }
+  // pt-br = aqui é aonde calculamos os dados como sequencia, timestamp, dados e HashAnterior.
+  //en = this is where we calculate the data such as sequence, timestamp, data and Previuos hash.
   payload: {
     sequence: number;
     timestamp: number;
+    data: any;
+    hashPrevious: string;
   }
 }
